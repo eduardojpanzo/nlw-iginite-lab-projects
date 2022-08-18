768363e82366c854651bc1aa2a5a51b3c4ea8f05
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2022-08-18 13:14:26 +0100

    Sala page created with HTML and CSS

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,43 @@
+import {useParams} from 'react-router-dom'
+import { Button } from '../components/Button';
+
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/room.scss';
+import { RoomCode } from '../components/RoomCode';
+
+type RoomParms = {
+    id:string;
+}
+
 export function Room(){
+    const parms = useParams<RoomParms>();
     return(
-        <h1>Salas</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="letmeask" />
+                    <RoomCode code={parms.id!}/>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form >
+                    <textarea
+                        placeholder='O que você quer perguntar?'
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type='submit'>Enviar Pergunta</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     )
 }
