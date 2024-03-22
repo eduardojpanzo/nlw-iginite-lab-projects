a39dc2e992af6591053adc18e8d80a716a8c49d5
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-03-22 20:47:13 +0100

    feat: init again

--- src/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-
-export function useAuth(){
-    const value = useContext(AuthContext);
-
-    return value;
-}
